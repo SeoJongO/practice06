03ef29c3de05a5c5c48a0c031b3ed4beacc4e3d6
SeoJongO <eve7884dbqls@naver.com>
2021-06-09 16:07:38 +0900

    상속 예제

---
+++ src/com/javaex/ex04/User.java
@@ -0,0 +1,47 @@
+package com.javaex.ex04;
+
+public class User {
+
+	protected String id;
+	protected String password;
+	protected String name;
+
+	public User() {
+
+	}
+
+	public User(String id, String password, String name) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassoword() {
+		return password;
+	}
+
+	public void setPassoword(String passoword) {
+		this.password = passoword;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void showInfo() {
+
+	}
+
+}
